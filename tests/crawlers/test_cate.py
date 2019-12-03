b3316d7fed25699f008ce1199b08954546833c85
ssfdust <ssfdust@gmail.com>
2019-12-03 18:04:32 +0800

    limit requests

--- tests/crawlers/test_cate.py
+++ tests/crawlers/test_cate.py
@@ -10,8 +10,9 @@ from blocked_domain_generator.crawlers.cate import MoreSiteCrawler
 async def test_indirect_crawler(request):
     extract = request.config.cache.get("index", {})
     more_site_crawler = MoreSiteCrawler(extract)
+    more_site_crawler.node_tuple_lst = more_site_crawler.node_tuple_lst[:10]
     await more_site_crawler.load_website()
     more_site_crawler.parse()
     records = more_site_crawler.to_records()
     assert records
-    extract = request.config.cache.set("records", records)
+    extract = request.config.cache.set("records", records[:10])
